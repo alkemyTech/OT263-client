f0e6a1fb00e117f10398206349191e696f217c65
Alan Coronado <contact@alancoronado.com>
2022-08-26 12:57:51 -0700

    Feature/OT263-22 Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import Header from './Components/Header/Header'
 import AppRoutes from './Components/Routes/AppRoutes'
 
 function App() {
 	return (
 		<div className='App'>
 			<BrowserRouter>
+				<Header/>
 				<AppRoutes />
 			</BrowserRouter>
 		</div>

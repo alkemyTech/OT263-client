71c4714d462698b76ea21e337c4132450e31ac32
Sebastian Garcia Nallar <sebagn@hotmail.com>
2022-08-28 11:55:16 -0300

    fixed img style and fontfamily

--- src/Components/NewsDetail/NewsDetail.js
+++ src/Components/NewsDetail/NewsDetail.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import axios from "axios";
 import Loader from "../Loader/Loader";
 import "./NewsDetail.css";
 
@@ -14,13 +13,14 @@ const NewsDetail = () => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                // const result = await axios(`http://localhost:3000/news/${id}`);
+
+                // Replace the following line with GET public service API call
                 const result = {
                     data: {
                         name: "News Title",
                         content:
                             "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum                     ",
-                        image: "https://via.placeholder.com/1500x500.png",
+                        image: "https://via.placeholder.com/2000x800.png",
                     },
                 };
                 setNews(result.data);

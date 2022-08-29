5c594d6d39d64ed716cb0c2429ce8388cdab3395
Fuku <fukushimafederico@hotmail.com>
2022-08-29 00:11:57 -0300

    armo esqueleto del home y agrego el slider

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,56 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios';
+import FlatList from 'flatlist-react';
+import SliderContainer from '../Components/Slider/SliderContainer'
 
 const Home = () => {
-	return <div>Hola! Bienvenidx</div>
+  const [listOfNews, setListOfNews] = useState([{image:"image", texto: "texto"}])
+
+  // useEffect(() => {
+  //   axios.get("http://localhost:3001/RUTA-DE-NEWS-").then((response) => {
+  //     setListOfNews(response.data)
+  //   })
+  // }, [])
+
+  const renderNews= (value, index) => {
+    return(
+      <div className='mainContainer'>
+        <div className='card'>
+          <div className='imageContainer'>
+            <div>imagen - {value.image}</div>
+          </div>
+          <div className='newContainer'>
+            <div>novedad - {value.texto}</div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+	return <div className='rows'>
+    <div className='columns is-vcentered is-centered' style={{margin: 15}}>
+      <div className='rows column is-5' style={{marginRight: 5}}>
+        <h1 className='row title'>Hola! Bienvenidx</h1>
+        <h3 className='row'>texto de bienvenida</h3>
+        <button className='row'>Contactenos</button>
+      </div>
+      <div className='column is-5' style={{marginLeft: 5}}>
+        <div><SliderContainer/></div>
+      </div>
+    </div>
+    <div>Staff</div>
+    <div>Testimonios</div>
+    <div>
+      Novedades
+      <FlatList 
+        list={listOfNews}
+        renderItem={renderNews}
+        reversed
+      />
+    </div>
+    
+    
+  </div>
 }
 
 export default Home

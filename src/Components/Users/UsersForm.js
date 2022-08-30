e55109d5d25e38d07c5ee195eeb5fccadda06867
Sebastian Garcia Nallar <sebagn@hotmail.com>
2022-08-30 18:34:46 -0300

    changed UsersForm to Formik and validation with Yup

--- src/Components/Users/UsersForm.js
+++ src/Components/Users/UsersForm.js
@@ -1,38 +1,69 @@
-import React, { useState } from 'react';
-import '../FormStyles.css';
+import React from "react";
+import { Form, Formik, useField } from "formik";
+import axios from "axios";
+import { userEditSchema } from "../../schemas";
 
-const UserForm = () => {
-    const [initialValues, setInitialValues] = useState({
-        name: '',
-        email: '',
-        roleId: ''
-    })
+import "../FormStyles.css";
 
-    const handleChange = (e) => {
-        if(e.target.name === 'name'){
-            setInitialValues({...initialValues, name: e.target.value})
-        } if(e.target.name === 'email'){
-            setInitialValues({...initialValues, email: e.target.value})
-        }
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(initialValues);
-    }
+const UserForm = (user) => {
+    const MyTextInput = ({ label, ...props }) => {
+        const [field, meta] = useField(props);
+        return (
+            <>
+                <label htmlFor={props.id || props.name}>{label}</label>
+                <input className="text-input" {...field} {...props} />
+                {meta.touched && meta.error ? (
+                    <div className="error">{meta.error}</div>
+                ) : null}
+            </>
+        );
+    };
+    const MySelect = ({ label, ...props }) => {
+        const [field, meta] = useField(props);
+        return (
+            <div>
+                <label htmlFor={props.id || props.name}>{label}</label>
+                <select {...field} {...props} />
+                {meta.touched && meta.error ? (
+                    <div className="error">{meta.error}</div>
+                ) : null}
+            </div>
+        );
+    };
 
     return (
-        <form className="form-container" onSubmit={handleSubmit}>
-            <input className="input-field" type="text" name="name" value={initialValues.name || ''} onChange={handleChange} placeholder="Name"></input>
-            <input className="input-field" type="text" name="email" value={initialValues.description || ''} onChange={handleChange} placeholder="Email"></input>
-            <select className="input-field" value={initialValues.roleId || ''} onChange={e => setInitialValues({...initialValues, roleId: e.target.value})}>
-                <option value="" disabled >Select the role</option>
-                <option value="1">Admin</option>
-                <option value="2">User</option>
-            </select>
-            <button className="submit-btn" type="submit">Send</button>
-        </form>
+        <Formik
+            initialValues={{
+                name: user.name,
+                lastName: user.lastName,
+                roleId: user.roleId,
+            }}
+            validationSchema={userEditSchema}
+            onSubmit={(values, { setSubmitting }) => {
+                axios.post("http://localhost:3000/users/" + user.id, values);
+                setSubmitting(false);
+            }}
+        >
+            <Form>
+                <MyTextInput
+                    label="Nombre"
+                    type="text"
+                    name="name"
+                    placeholder="Nombre"
+                />
+                <MyTextInput
+                    label="Apellido"
+                    type="text"
+                    name="lastName"
+                    placeholder="Apellido"
+                />
+                <MySelect label="Rol" name="roleId">
+                    <option value="1">Administrador</option>
+                    <option value="2">Usuario</option>
+                </MySelect>
+            </Form>
+        </Formik>
     );
-}
- 
+};
+
 export default UserForm;
--- src/schemas/index.js
+++ src/schemas/index.js
@@ -1,9 +1,23 @@
-import * as yup from 'yup';
+import * as yup from "yup";
 
 export const registerSchema = yup.object().shape({
-  firstName: yup.string().required('Este campo es obligatorio'),
-  lastName: yup.string().required('Este campo es obligatorio'),
-  email: yup.string().email('Ingrese un email valido').required('Este campo es obligatorio'),
-  password: yup.string().min(6,  'Debe ser mínimo 6 caracteres').required('Este campo es obligatorio'),
+    firstName: yup.string().required("Este campo es obligatorio"),
+    lastName: yup.string().required("Este campo es obligatorio"),
+    email: yup
+        .string()
+        .email("Ingrese un email valido")
+        .required("Este campo es obligatorio"),
+    password: yup
+        .string()
+        .min(6, "Debe ser mínimo 6 caracteres")
+        .required("Este campo es obligatorio"),
 });
 
+export const userEditSchema = yup.object().shape({
+    firstName: yup.string().required("Este campo es obligatorio"),
+    lastName: yup.string().required("Este campo es obligatorio"),
+    roleId: yup
+        .mixed()
+        .oneOf(["Administrador", "Usuario"], "Debe seleccionar un rol")
+        .required("Este campo es obligatorio"),
+});

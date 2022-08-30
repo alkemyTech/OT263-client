45cda321597e6dd3515c661b42058dc6657cd6cd
Fuku <fukushimafederico@hotmail.com>
2022-08-30 03:55:03 -0300

    preparo la seccion novedades y le agrego un poco de estilos

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,9 +2,15 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios';
 import FlatList from 'flatlist-react';
 import SliderContainer from '../Components/Slider/SliderContainer'
+import { TbH1 } from 'react-icons/tb';
+
+
 
 const Home = () => {
-  const [listOfNews, setListOfNews] = useState([{image:"image", texto: "texto"}])
+  const [listOfNews, setListOfNews] = useState([
+    {image: "image",
+      content:"Este es un ejemplo de una gran novedad novedosa"}
+  ])
 
   // useEffect(() => {
   //   axios.get("http://localhost:3001/RUTA-DE-NEWS-").then((response) => {
@@ -14,15 +20,34 @@ const Home = () => {
 
   const renderNews= (value, index) => {
     return(
-      <div className='mainContainer'>
-        <div className='card'>
-          <div className='imageContainer'>
-            <div>imagen - {value.image}</div>
+      <div className='mainContainer'
+        style={{display:"flex", alignItems:"center"}}
+      >
+        <div className='card mr-5 p-1' 
+          style={{display:"flex", borderRadius: 20, backgroundColor: "#6d7aee", width:350}}>
+          <div className='imageContainer column card-image'>
+            <figure class="image" style={{width: 150}}>
+              <img style={{borderRadius: 20, height: 158}} src={'/images/placeholder/150x150.png'} alt="imagen"/>
+            </figure>
           </div>
-          <div className='newContainer'>
-            <div>novedad - {value.texto}</div>
+          <div className='contentContainer column card-content'>
+            <div className='content'>{value.content}</div>
           </div>
         </div>
+
+        <div className='card mr-5 p-1' 
+          style={{display:"flex", borderRadius: 20, backgroundColor: "#6d7aee", width:350}}>
+          <div className='imageContainer column card-image'>
+            <figure class="image" style={{width: 150}}>
+              <img style={{borderRadius: 20, height: 158}} src={'/images/placeholder/150x150.png'} alt="imagen"/>
+            </figure>
+          </div>
+          <div className='contentContainer column card-content'>
+            <div className='content'>{value.content}</div>
+          </div>
+        </div>
+
+
       </div>
     )
   }
@@ -38,18 +63,46 @@ const Home = () => {
         <div><SliderContainer/></div>
       </div>
     </div>
-    <div>Staff</div>
-    <div>Testimonios</div>
+
+    <div className='container'>
     <div>
-      Novedades
-      <FlatList 
-        list={listOfNews}
-        renderItem={renderNews}
-        reversed
-      />
+        <h2 className='subtitle is-4' style={{color:"black", fontWeight:"800" }}>Nuestro Staff</h2>
+      </div>
+    <div 
+        className='container is-centered' 
+        style={{height: 240, display: "flex", justifyContent:"start",alignItems: "center"}}
+      >
+        
+      </div>
     </div>
-    
-    
+
+    <div className='container'>
+    <div>
+        <h2 className='subtitle is-4' style={{color:"black", fontWeight:"800" }}>Testimonios</h2>
+      </div>
+    <div 
+        className='container is-centered' 
+        style={{height: 240, display: "flex", justifyContent:"start",alignItems: "center"}}
+      >
+        
+      </div>
+    </div>
+
+    <div className='container'>
+      <div>
+        <h2 className='subtitle is-4' style={{color:"black", fontWeight:"800" }}>Ultimas Novedades</h2>
+      </div>
+      <div 
+        className='container is-centered' 
+        style={{height: 230, display: "flex", justifyContent:"start",alignItems: "center"}}
+      >
+        <FlatList 
+          list={listOfNews}
+          renderItem={renderNews}
+          reversed
+        />
+      </div>
+    </div>
   </div>
 }
 

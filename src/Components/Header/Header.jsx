b0707f1c92873af06e2750925e080f04ac504343
Sebastian Garcia Nallar <sebagn@hotmail.com>
2022-09-06 14:51:12 -0300

    replaced elements <a> with <Links> in header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,9 @@
+import React from 'react';
 import 'bulma/css/bulma.min.css';
 import logo from './Group-33.png';
 import { routes } from '../../Config/routes';
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
+import { Link } from "react-router-dom";
 
 const Header =()=> {
     const user = useSelector(state => state.user.currentUser);
@@ -18,38 +20,38 @@ const Header =()=> {
     return(
         <nav className="navbar has-navbar-fixed-top has-shadow" role='navigation'>
             <div className="navbar-brand">
-                <a className="navbar-item">
+                <Link to={routes.home} className="navbar-item">
                     <img src={logo} alt="" width="100" height="52" />
-                </a>
-                <a role="button" id='burger' className="navbar-burger burger" data-target="navMenu" aria-label="menu" aria-expanded="false" onClick={toggleBurger}>
+                </Link>
+                <button id='burger' className="navbar-burger burger" data-target="navMenu" aria-label="menu" aria-expanded="false" onClick={toggleBurger}>
                     <span ></span>
                     <span ></span>
                     <span ></span>
-                </a>
+                </button>
             </div>
             <div id="navMenu" className="navbar-menu">
                 <div className="navbar-end">
-                    <a className="navbar-item" style={{"font-weight":"bold"}} href={routes.home}>Inicio</a>
-                    <a className="navbar-item" href={routes.about}>Nosotros</a>
-                    <a className="navbar-item" href={routes.news}>Novedades</a>
-                    <a className="navbar-item" href={routes.testimonials}>Testimonios</a>
-                    <a className="navbar-item" href={routes.contact}>Contacto</a>
-                    <a className="navbar-item" href={routes.getInvolved}>Contribuye</a>
+                    <Link to={routes.home} className="navbar-item" style={{"font-weight":"bold"}} >Inicio</Link>
+                    <Link to={routes.about} className="navbar-item" >Nosotros</Link>
+                    <Link to={routes.news} className="navbar-item" > Novedades</Link>
+                    <Link to={routes.testimonials} className="navbar-item" >Testimonios</Link>
+                    <Link to={routes.contact} className="navbar-item" >Contacto</Link>
+                    <Link to={routes.getInvolved} className="navbar-item" >Contribuye</Link>
                     {user 
                         ?   isAdmin 
-                            ?   <a className="navbar-item" href={routes.admin.root}>Backoffice</a>
-                            :   <a className="navbar-item" href={routes.profile}>Mi Perfil</a> 
+                            ?   <Link to={routes.admin.root} className="navbar-item" >Backoffice</Link>
+                            :   <Link to={routes.profile} className="navbar-item" >Mi Perfil</Link> 
                         :   <div className="navbar-item">
                                 <div className="field is-grouped">
                                     <p className="control">
-                                        <a className="button is-rounded" href={routes.login}>
+                                        <Link to={routes.login} className="button is-rounded" >
                                             <span>Log in</span>
-                                        </a>
+                                        </Link>
                                     </p>
                                     <p className="control">
-                                        <a className="button is-rounded" style={{"background-color":"#FF0000", color:"white"}} href={routes.signup}>
+                                        <Link to={routes.signup} className="button is-rounded" style={{"background-color":"#FF0000", color:"white"}} >
                                             <span>Registrate</span>
-                                        </a>
+                                        </Link>
                                     </p>
                                 </div>
                             </div>

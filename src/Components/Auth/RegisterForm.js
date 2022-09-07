b31e36103f077f54e74e51b410857339ddb33e3d
gabrielaNieves <gabrielapaez95@gmail.com>
2022-09-06 21:09:10 -0300

    Edit the newsForm components based on input required and add Rich text editor

--- src/Components/Auth/RegisterForm.js
+++ src/Components/Auth/RegisterForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useFormik } from 'formik';
-import { registerSchema } from '../../schemas';
+import { registerSchema } from '../../Schemas';
 
 
 const onSubmit = (values, actions) => {
--- src/Components/News/NewsForm.js
+++ src/Components/News/NewsForm.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import '../../Components/FormStyles.css';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+
 
 const NewsForm = () => {
     const [initialValues, setInitialValues] = useState({
@@ -9,12 +12,11 @@ const NewsForm = () => {
     });
 
     const handleChange = (e) => {
-        if(e.target.name === 'title'){
-            setInitialValues({...initialValues, title: e.target.value})
-        } if(e.target.name === 'content'){
-            setInitialValues({...initialValues, content: e.target.value})
-        } if(e.target.name === 'category') {
-            setInitialValues({...initialValues, category: e.target.value})
+        if (e.target.name === 'title') {
+            setInitialValues({ ...initialValues, title: e.target.value })
+        } 
+        if (e.target.name === 'category') {
+            setInitialValues({ ...initialValues, category: e.target.value })
         }
     }
 
@@ -23,19 +25,72 @@ const NewsForm = () => {
         console.log(initialValues);
     }
 
+    const handleCKEditor = (event, editor) => {
+        const data = editor.getData();
+        setInitialValues({...initialValues, content: data})
+    }
+
     return (
-        <form className="form-container" onSubmit={handleSubmit}>
-            <input className="input-field" type="text" name="title" value={initialValues.title || ''} onChange={handleChange}></input>
-            <input className="input-field" type="text" name="content" value={initialValues.content || ''} onChange={handleChange}></input>
-            <select className="select-field" name="category" value={initialValues.category || ''} onChange={handleChange}>
-                <option value="" disabled>Select category</option>
-                <option value="1">Demo option 1</option>
-                <option value="2">Demo option 2</option>
-                <option value="3">Demo option 3</option>
-            </select>
-            <button className="submit-btn" type="submit">Send</button>
-        </form>
+        <div className="container my-4">
+            <div className='columns is-mobile'>
+                <div className="column is-half-tablet is-offset-one-quarter-tablet is-full-mobile ">
+                    <form onSubmit={handleSubmit}>
+                        <label className='label'>Título</label>
+                        <div className="control">
+                            <input className="input" type="text" name='title' placeholder="Ingrese Título" onChange={handleChange} />
+                        </div>
+                        <label className='label'>Contenido</label>
+                        <CKEditor
+                        editor={ClassicEditor}
+                        data= 'Agrega lo que quieras aquíii'
+                        onInit={editor=> {
+
+                        }}
+                        onChange={handleCKEditor}
+                        
+                        />
+
+
+                        <label className='label'>Categoria</label>
+                        <div class="select">
+                            <select name='category' onChange={handleChange}>
+                                <option value="" disabled>Select category</option>
+                                <option value="1">Demo option 1</option>
+                                <option value="2">Demo option 2</option>
+                                <option value="3">Demo option 3</option>
+                            </select>
+                        </div>
+
+                        <p className="control">
+                            <button className="button is-primary" type='submit'>
+                                Enviar
+                            </button>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+
+        /*
+            <form className="form-container" onSubmit={handleSubmit}>
+                <input className="input-field" type="text" name="title" value={initialValues.title || ''} onChange={handleChange}></input>
+                <input className="input-field" type="text" name="content" value={initialValues.content || ''} onChange={handleChange}></input>
+                <select className="select-field" name="category" value={initialValues.category || ''} onChange={handleChange}>
+                    <option value="" disabled>Select category</option>
+                    <option value="1">Demo option 1</option>
+                    <option value="2">Demo option 2</option>
+                    <option value="3">Demo option 3</option>
+                </select>
+                <button className="submit-btn" type="submit">Send</button>
+            </form>
+            */
     );
 }
- 
+
 export default NewsForm;

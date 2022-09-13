ee119b760aa996241e2365c921121b39343dc118
Sebastian Garcia Nallar <sebagn@hotmail.com>
2022-09-13 20:37:26 -0300

    FIX navigation

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -1,12 +1,10 @@
 import 'bulma/css/bulma.min.css';
 import { Formik, Form, Field } from "formik";
 import { PostLogin } from "../../Services/privateApiService"
-import {useNavigate, Link} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import { loginSchema } from '../../schemas';
 import { logIn } from '../../actions/userActions';
 import { useDispatch } from 'react-redux';
-import { config } from '../../config';
-
 
 const LoginForm =()=> {
   const navigate = useNavigate()
@@ -36,7 +34,7 @@ const LoginForm =()=> {
                         }) => (
                             <Form className='box is-shadowless'>
                                 <p className='field mb-0 is-size-5'>Bienvenido</p>
-                                <Link to={config.signUp}><h6 className='field is-size-3 has-text-weight-semibold'>Inicia sesión en tu cuenta!</h6></Link>
+                                <h6 className='field is-size-3 has-text-weight-semibold'>Inicia sesión en tu cuenta!</h6>
                                 <Field className="field input" name="email" type="email" placeholder="Email" />
                                 {errors.email && touched.email ? <div>{errors.email}</div> : null}
                                 <Field className=" field input" name="password" type="password" placeholder="Contraseña" />

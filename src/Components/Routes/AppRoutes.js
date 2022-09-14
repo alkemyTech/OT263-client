b0853f260a5cc738701dc7dcba152c2e916fe811
Juan Manuel Maroñas <jmmaronas@gmail.com>
2022-09-14 08:47:12 -0300

    restaure validacion de ruta

--- src/Components/Routes/AppRoutes.js
+++ src/Components/Routes/AppRoutes.js
@@ -18,7 +18,7 @@ import { useSelector } from 'react-redux'
 
 const AppRoutes = () => {
 	const user = useSelector((state) => state.user.currentUser)
-	const isAdmin = true
+	const isAdmin = user?.roleId === 1
 
 	return (
 		<Routes>

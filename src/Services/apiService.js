f533ff5fd14fb8443268b9079937154b3a01f1fd
Sebastian Garcia Nallar <sebagn@hotmail.com>
2022-09-26 19:41:28 -0300

    apiService get fixed. not sending token

--- src/Services/apiService.js
+++ src/Services/apiService.js
@@ -1,31 +1,20 @@
-import axios from "axios";
+import axios from 'axios';
 
 const token = localStorage.getItem('token');
 
-export const get =async(url, data)=> {
-    //Si no hay token se ejecuta este bloque para las solicitudes get
-    if(!token){
-        try {
-            return axios.get(url, data);
-        } catch (error) {
-            return error;
-        }
-    }
-    //Si hay token se ejecuta este bloque para las solicitudes get usando el token
-    try {
-        return axios.get(url, data, {
-            headers:{"Authorization" : `Bearer ${token}`}
-        });
-    } catch (error) {
-        return error;
-    }
-}
+export const get = (url) => {
+    if (!token) return axios.get(url);
 
-export const post =async(url, data)=> {
+    return axios.get(url, {
+        headers: { Authorization: `Bearer ${token}` },
+    });
+};
+
+export const post = async (url, data) => {
     //Si no hay token se ejecuta este bloque para las solicitudes post
-    if(!token){
+    if (!token) {
         try {
-            return axios.post(url, data);   
+            return axios.post(url, data);
         } catch (error) {
             return error;
         }
@@ -33,18 +22,18 @@ export const post =async(url, data)=> {
     //Si hay token se ejecuta este bloque para las solicitudes post usando el token
     try {
         return axios.post(url, data, {
-            headers:{"Authorization" : `Bearer ${token}`}
-        });   
+            headers: { Authorization: `Bearer ${token}` },
+        });
     } catch (error) {
         return error;
     }
-}
+};
 
-export const put =async(url, data)=> {
+export const put = async (url, data) => {
     //Si no hay token se ejecuta este bloque para las solicitudes post
-    if(!token){
+    if (!token) {
         try {
-            return axios.put(url, data);   
+            return axios.put(url, data);
         } catch (error) {
             return error;
         }
@@ -52,9 +41,9 @@ export const put =async(url, data)=> {
     //Si hay token se ejecuta este bloque para las solicitudes post usando el token
     try {
         return axios.put(url, data, {
-            headers:{"Authorization" : `Bearer ${token}`}
-        });   
+            headers: { Authorization: `Bearer ${token}` },
+        });
     } catch (error) {
         return error;
     }
-}
+};

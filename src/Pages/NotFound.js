cd30caa1443240197775c20f3b1ad05bc20f944b
Pablo Pastorino <phpastorino@outlook.com>
2022-09-26 15:41:05 -0300

    Adds NotFound page

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import Logo from '../Components/Common/Logo'
 
 const NotFound = () => {
-  return <div>NotFound</div>
+  return (
+    <section className='hero is-medium'>
+      <div className='hero-body'>
+        <div className='container has-text-centered'>
+          <div>
+            <Logo />
+          </div>
+          <p className='title'>Error 404 - No encontramos nada aquí</p>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default NotFound
